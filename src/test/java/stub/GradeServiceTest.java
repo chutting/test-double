c2b91a23083e040ba62215e9b6e64d81e184bd5e
chutting <tingting.chu@thoughtworks.com>
2020-09-15 15:21:19 +0800

    [Chu] feat: complete the stub feature

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -14,14 +14,23 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
 
+    @Mock
+    private GradeSystem gradeSystem;
+
+    @InjectMocks
+    private GradeService gradeService;
 
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        //Assertions.assertEquals(90.0, result);
+        when(gradeSystem.gradesFor(anyLong()))
+            .thenReturn(Arrays.asList(80.0, 90.0, 100.0));
+
+        assertEquals(90.0, gradeService.calculateAverageGrades(anyLong()));
     }
 }
